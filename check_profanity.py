17a3ec9651604b70f4046835e530512605eb034b
George Beinas <gbeinas@di.uoa.gr>
2015-12-08 23:13:39 +0200

    Initial code about reading from a file

---
+++ check_profanity.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1253 -*-
+
+
+def read_text():
+    quotes = open("C:\Users\√…Ÿ—√œ”\Desktop\Programming_Foundations_with_Python\movie_quotes.txt")
+    file_content = quotes.read()
+    print("The content of the txt file is " + file_content)
+
+read_text()
