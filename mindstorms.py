51b0100bb095c37938a618e28bca96c5f3eaa0ab
George Beinas <gbeinas@di.uoa.gr>
2015-12-08 14:38:18 +0200

    final changes.This file creates a square and a turtle

---
+++ mindstorms.py
@@ -0,0 +1,27 @@
+import turtle
+
+def draw_square(some_turtle):
+    i = 0
+    while i < 4:
+        some_turtle.forward(100)
+        some_turtle.right(90)
+        i = i+1
+        
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    #Create the turtle Brad - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    draw_square(brad)
+    #Create the turtle Angie - Draws a circle
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+    
+    window.exitonclick()
+
+draw_art()
